4c075965456ca8c77f0118a7fdfb2f38fa1becb2
imoderiii <893248267@qq.com>
2022-08-23 01:03:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-r471ha
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-r471ha)
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Test1 />
+  </div>
+</template>
+
+<script>
+import Test1 from './components/demo01/Test1.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Test1,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
